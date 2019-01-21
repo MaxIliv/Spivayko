1c282ddbfc9f8ad1e4562eadcac8b4b8eee65b36
Max Iliv <max.r.iliv@gmail.com>
2019-01-21 11:44:59 +0200

    regards, team, header

--- src/js/imageViewer/index.js
+++ src/js/imageViewer/index.js
@@ -66,3 +66,16 @@ var imageViewer = (function($){
     }
 
 })(jQuery);
+
+$(window).ready(function() {
+    var $img = $(document).find('.galery__item img');
+
+    $img.each(function(index, item) {
+        $(item).on('click', function(e) {
+            imageViewer.show( {
+                src: this.src,
+                description: this.alt
+            });
+        })
+    })
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,37 @@
-$(window).ready(function() {
-    var $img = $(document).find('.galery__item img');
+var $window = $(window);
+var $body = $('body');
+var pieces = {
+    header: $body.find('nav.nav'),
+    loader: $body.find('.loader')
+};
 
-    $img.each(function(index, item) {
-        $(item).on('click', function(e) {
-            imageViewer.show( {
-                src: this.src,
-                description: this.alt
-            });
-        })
-    })
-})
+$window.on('scroll', function() {
+    checkHeader();
+});
+
+(function() {
+    checkHeader();
+    hideLoader();    
+})();
+
+function checkHeader() {
+    if($window.scrollTop() > 30) {
+        if(pieces.header.hasClass('nav--bg')) {
+            return;
+        }
+
+        pieces.header.addClass('nav--bg');
+        pieces.header.removeClass('nav--top');
+    } else {
+        if(pieces.header.hasClass('nav--top')) {
+            return;
+        }
+
+        pieces.header.removeClass('nav--bg');
+        pieces.header.addClass('nav--top');
+    }
+}
+
+function hideLoader() {
+    setTimeout(() => pieces.loader.hide(), 400);
+}
