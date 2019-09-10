61faec015bf250f7f8480309972128f7215ba393
Max Iliv <max.r.iliv@gmail.com>
2019-09-10 18:03:59 +0300

    Updaaaaate

--- webpack.config.js
+++ webpack.config.js
@@ -7,13 +7,19 @@ var path = require('path');
 
 const MinifyPlugin = require("babel-minify-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
+
+const PATHS = {
+  src: path.join(__dirname, 'src'), //absolute path to RepoDir/src
+  dist: path.join(__dirname, 'dist') //absolute path to RepoDir/dist
+}
 
 module.exports = {
   entry: './src/js/main.js',
   mode: 'none',
   output: {
-      path: path.resolve(__dirname, 'build'),
-      filename: 'js/app.bundle.js'
+      path: PATHS.dist,
+      filename: 'src/js/app.bundle.js'
   },
   module: {
     rules: [
@@ -28,12 +34,7 @@ module.exports = {
             test: /\.scss$/,
             use: [
               {
-                loader: MiniCssExtractPlugin.loader,
-                options: {
-                  // you can specify a publicPath here
-                  // by default it use publicPath in webpackOptions.output
-                  publicPath: './build/css'
-                }
+                loader: MiniCssExtractPlugin.loader
               },
               // "style-loader",
                "css-loader", "postcss-loader", "sass-loader"]
@@ -48,9 +49,15 @@ module.exports = {
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    })
+      filename: 'src/css/[name].css',
+      chunkFilename: "src/css/[id].css"
+    }),
+    new CopyPlugin([
+      { from: 'static', to: 'static' },
+      { from: 'src/css/vendor', to: 'src/css/vendor' },
+      { from: 'fonts', to: 'fonts' },
+      { from: 'index.html', to: 'index.html' },
+    ]),
   ],
   // devtool: 'eval-source-map',
   watch: false
